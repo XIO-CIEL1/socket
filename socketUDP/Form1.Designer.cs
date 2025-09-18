94d6c224d6046e0d759cf6138da9b06bcc28f044
Tom Xiong <xiongtom151@gmail.com>
2025-09-18 11:42:44 +0200

    #2 Création interface et insertion primitives (grandes lignes du programme)

--- socketUDP/Form1.Designer.cs
+++ socketUDP/Form1.Designer.cs
@@ -28,13 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.txtLocalIP = new System.Windows.Forms.TextBox();
+            this.txtLocalPort = new System.Windows.Forms.TextBox();
+            this.lblDest = new System.Windows.Forms.Label();
+            this.txtDestIP = new System.Windows.Forms.TextBox();
+            this.txtDestPort = new System.Windows.Forms.TextBox();
+            this.lblEnvoi = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.lblRecv = new System.Windows.Forms.Label();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.btnCreateBind = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lblLocalPort = new System.Windows.Forms.Label();
+            this.lblDestPort = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.Location = new System.Drawing.Point(12, 15);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(36, 13);
+            this.lblLocal.TabIndex = 0;
+            this.lblLocal.Text = "Recp.";
+            // 
+            // txtLocalIP
+            // 
+            this.txtLocalIP.Location = new System.Drawing.Point(70, 12);
+            this.txtLocalIP.Name = "txtLocalIP";
+            this.txtLocalIP.Size = new System.Drawing.Size(120, 20);
+            this.txtLocalIP.TabIndex = 1;
+            this.txtLocalIP.Text = "127.0.0.1";
+            this.txtLocalIP.TextChanged += new System.EventHandler(this.txtLocalIP_TextChanged);
+            // 
+            // txtLocalPort
+            // 
+            this.txtLocalPort.Location = new System.Drawing.Point(228, 12);
+            this.txtLocalPort.Name = "txtLocalPort";
+            this.txtLocalPort.Size = new System.Drawing.Size(60, 20);
+            this.txtLocalPort.TabIndex = 2;
+            this.txtLocalPort.Text = "3031";
+            // 
+            // lblDest
+            // 
+            this.lblDest.AutoSize = true;
+            this.lblDest.Location = new System.Drawing.Point(12, 45);
+            this.lblDest.Name = "lblDest";
+            this.lblDest.Size = new System.Drawing.Size(32, 13);
+            this.lblDest.TabIndex = 4;
+            this.lblDest.Text = "Dest.";
+            // 
+            // txtDestIP
+            // 
+            this.txtDestIP.Location = new System.Drawing.Point(70, 42);
+            this.txtDestIP.Name = "txtDestIP";
+            this.txtDestIP.Size = new System.Drawing.Size(120, 20);
+            this.txtDestIP.TabIndex = 5;
+            this.txtDestIP.Text = "127.0.0.1";
+            // 
+            // txtDestPort
+            // 
+            this.txtDestPort.Location = new System.Drawing.Point(228, 42);
+            this.txtDestPort.Name = "txtDestPort";
+            this.txtDestPort.Size = new System.Drawing.Size(60, 20);
+            this.txtDestPort.TabIndex = 6;
+            this.txtDestPort.Text = "3032";
+            // 
+            // lblEnvoi
+            // 
+            this.lblEnvoi.AutoSize = true;
+            this.lblEnvoi.Location = new System.Drawing.Point(12, 80);
+            this.lblEnvoi.Name = "lblEnvoi";
+            this.lblEnvoi.Size = new System.Drawing.Size(34, 13);
+            this.lblEnvoi.TabIndex = 8;
+            this.lblEnvoi.Text = "Envoi";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(70, 77);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(218, 20);
+            this.txtSend.TabIndex = 9;
+            this.txtSend.Text = "Bonjour UDP";
+            // 
+            // lblRecv
+            // 
+            this.lblRecv.AutoSize = true;
+            this.lblRecv.Location = new System.Drawing.Point(12, 113);
+            this.lblRecv.Name = "lblRecv";
+            this.lblRecv.Size = new System.Drawing.Size(36, 13);
+            this.lblRecv.TabIndex = 10;
+            this.lblRecv.Text = "Recp.";
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(70, 110);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtReceive.Size = new System.Drawing.Size(218, 180);
+            this.txtReceive.TabIndex = 11;
+            // 
+            // btnCreateBind
+            // 
+            this.btnCreateBind.Location = new System.Drawing.Point(315, 10);
+            this.btnCreateBind.Name = "btnCreateBind";
+            this.btnCreateBind.Size = new System.Drawing.Size(200, 25);
+            this.btnCreateBind.TabIndex = 12;
+            this.btnCreateBind.Text = "Créer Socket et Bind(IPEr)";
+            this.btnCreateBind.UseVisualStyleBackColor = true;
+            this.btnCreateBind.Click += new System.EventHandler(this.btnCreateBind_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(315, 45);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(200, 25);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Fermer Close()";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(315, 75);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(200, 25);
+            this.btnSend.TabIndex = 14;
+            this.btnSend.Text = "Envoyer SendTo(IPEd)";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(315, 110);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(200, 25);
+            this.btnReceive.TabIndex = 15;
+            this.btnReceive.Text = "Recevoir ReceiveFrom()";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(315, 265);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 25);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "CLS";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lblLocalPort
+            // 
+            this.lblLocalPort.AutoSize = true;
+            this.lblLocalPort.Location = new System.Drawing.Point(196, 15);
+            this.lblLocalPort.Name = "lblLocalPort";
+            this.lblLocalPort.Size = new System.Drawing.Size(26, 13);
+            this.lblLocalPort.TabIndex = 3;
+            this.lblLocalPort.Text = "Port";
+            // 
+            // lblDestPort
+            // 
+            this.lblDestPort.AutoSize = true;
+            this.lblDestPort.Location = new System.Drawing.Point(196, 45);
+            this.lblDestPort.Name = "lblDestPort";
+            this.lblDestPort.Size = new System.Drawing.Size(26, 13);
+            this.lblDestPort.TabIndex = 7;
+            this.lblDestPort.Text = "Port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(534, 311);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnCreateBind);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.lblRecv);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lblEnvoi);
+            this.Controls.Add(this.lblDestPort);
+            this.Controls.Add(this.txtDestPort);
+            this.Controls.Add(this.txtDestIP);
+            this.Controls.Add(this.lblDest);
+            this.Controls.Add(this.lblLocalPort);
+            this.Controls.Add(this.txtLocalPort);
+            this.Controls.Add(this.txtLocalIP);
+            this.Controls.Add(this.lblLocal);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Communication par socket UDP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.TextBox txtLocalIP;
+        private System.Windows.Forms.TextBox txtLocalPort;
+        private System.Windows.Forms.Label lblDest;
+        private System.Windows.Forms.TextBox txtDestIP;
+        private System.Windows.Forms.TextBox txtDestPort;
+        private System.Windows.Forms.Label lblEnvoi;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Label lblRecv;
+        private System.Windows.Forms.TextBox txtReceive;
+        private System.Windows.Forms.Button btnCreateBind;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label lblLocalPort;
+        private System.Windows.Forms.Label lblDestPort;
     }
 }
-
--- socketUDP/Form1.cs
+++ socketUDP/Form1.cs
@@ -7,14 +7,157 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
 
 namespace socketUDP
 {
     public partial class Form1 : Form
     {
+        private Socket _socket;
+
         public Form1()
         {
             InitializeComponent();
+            UpdateButtons();
+        }
+
+        private void UpdateButtons()
+        {
+            bool opened = _socket != null;
+            if (this.Controls.Count == 0) return; // designer-time safeguard
+            // Enable/disable buttons based on socket state
+            var btnCreate = this.Controls.Find("btnCreateBind", true).FirstOrDefault() as Button;
+            var btnClose = this.Controls.Find("btnClose", true).FirstOrDefault() as Button;
+            var btnSend = this.Controls.Find("btnSend", true).FirstOrDefault() as Button;
+            var btnReceive = this.Controls.Find("btnReceive", true).FirstOrDefault() as Button;
+
+            if (btnCreate != null) btnCreate.Enabled = !opened;
+            if (btnClose != null) btnClose.Enabled = opened;
+            if (btnSend != null) btnSend.Enabled = opened;
+            if (btnReceive != null) btnReceive.Enabled = opened;
+        }
+
+        private void btnCreateBind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket != null)
+                {
+                    AppendRecvLine("Socket déjà créé.");
+                    return;
+                }
+
+                var localIP = IPAddress.Parse(txtLocalIP.Text.Trim());
+                int localPort = int.Parse(txtLocalPort.Text.Trim());
+                IPEndPoint localEP = new IPEndPoint(localIP, localPort);
+
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+                _socket.ReceiveTimeout = 2000; // 2s timeout for blocking ReceiveFrom
+                _socket.Bind(localEP);
+
+                AppendRecvLine($"Bind OK sur {localEP}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Erreur création/bind", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                _socket = null;
+            }
+            finally
+            {
+                UpdateButtons();
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (_socket != null)
+                {
+                    _socket.Close();
+                    _socket = null;
+                    AppendRecvLine("Socket fermé.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Erreur fermeture", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                UpdateButtons();
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (_socket == null)
+            {
+                MessageBox.Show(this, "Créez et bindez d'abord la socket.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                var destIP = IPAddress.Parse(txtDestIP.Text.Trim());
+                int destPort = int.Parse(txtDestPort.Text.Trim());
+                EndPoint destEP = new IPEndPoint(destIP, destPort);
+
+                byte[] msg = Encoding.ASCII.GetBytes(txtSend.Text ?? string.Empty);
+                int sent = _socket.SendTo(msg, destEP);
+                AppendRecvLine($"Envoyé {sent} octets à {destEP}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Erreur envoi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            if (_socket == null)
+            {
+                MessageBox.Show(this, "Créez et bindez d'abord la socket.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            try
+            {
+                byte[] buffer = new byte[1024];
+                EndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+                int len = _socket.ReceiveFrom(buffer, ref remoteEP);
+                string txt = Encoding.ASCII.GetString(buffer, 0, len);
+                AppendRecvLine($"De {remoteEP} -> {txt}");
+            }
+            catch (SocketException sx)
+            {
+                if (sx.SocketErrorCode == SocketError.TimedOut)
+                {
+                    AppendRecvLine("Réception: timeout.");
+                }
+                else
+                {
+                    MessageBox.Show(this, sx.Message, "Erreur réception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Erreur réception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtReceive.Clear();
+        }
+
+        private void AppendRecvLine(string text)
+        {
+            txtReceive.AppendText(text + Environment.NewLine);
+        }
+
+        private void txtLocalIP_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
